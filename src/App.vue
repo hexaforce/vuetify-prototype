0ac388d79ef64708e601ed90f57e44e930b62ca1
tomokazu.tantaka <tantaka.tomokazu@gmail.com>
2019-06-15 01:35:53 +0900

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app" dark>
+    <v-app>
+      <v-content>
+        <router-view/>
+        <BottomNav/>
+      </v-content>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import BottomNav from "@/components/BottomNav.vue"
+export default {
+  name: "app",
+  components: {
+    BottomNav
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <div class="home">
+      <v-img :src="require('../assets/Vue.js.svg')" class="my-3" contain height="175"></v-img>
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+  </v-container>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue"
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+}
+</script>
