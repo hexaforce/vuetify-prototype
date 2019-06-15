86d1359037cfe8f3fbbb909f137dce2b1b324dfc
tomokazu.tantaka <tantaka.tomokazu@gmail.com>
2019-06-15 12:22:07 +0900

    test

--- src/views/Example2.vue
+++ src/views/Example2.vue
@@ -1,17 +1,36 @@
 <template>
   <v-container>
-    <div class="about">
-      <h1>This is an about page2</h1>
-    </div>
+    <canvas id="planet-chart"></canvas>
   </v-container>
 </template>
 
 <script>
-//import HelloWorld from "@/components/HelloWorld.vue"
+import Chart from "chart.js"
+import planetChartData from "./chart-data.js"
+
 export default {
   name: "example2",
-  components: {
-    // HelloWorld
+  components: {},
+  data() {
+    return {
+      planetChartData: planetChartData
+    }
+  },
+  mounted() {
+    this.createChart("planet-chart", this.planetChartData)
+  },
+  methods: {
+    createChart(chartId, chartData) {
+
+      const ctx = document.getElementById(chartId)
+
+      const myChart = new Chart(ctx, {
+        type: chartData.type,
+        data: chartData.data,
+        options: chartData.options
+      })
+
+    }
   }
 }
 </script>
